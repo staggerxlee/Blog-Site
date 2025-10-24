14d85aeb8607b2f46e8bec7b5c828d51a3d61edd
Your Name <saswatkc123@gmail.com>
2025-10-24 20:49:48 +0545

    Using dynamic data for posts,  creating databases for posts through model, adding data to databases through python ORM, browsing through ORM, using that data to display in the site instead of dummy data, registering models in admin.py for models to show up in admin page for easy CRUD opearations

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Post(models.Model):
+    title =models.CharField(max_length=100)
+    content=models.TextField()
+    date_posted=models.DateTimeField(default=timezone.now)
+    author= models.ForeignKey(User,on_delete=models.CASCADE) #what cascade does is when you delete the user, it deletes all its relative data, posts and etc.
+
+    def __str__(self): #self is the current object that you are accessing right now, like if your accessing post 1, then self=post 1 object
+        return self.title #returns the title of the currenly accessed object's title
+  
--- blog/views.py
+++ blog/views.py
@@ -1,33 +1,11 @@
-from django.shortcuts import render
-
-posts=[
-    {
-        'author':'CoreyMS',
-        'title':'First Post',
-        'content':'Hey guys!',
-        'date_posted':'August 27,2023'
-    },
-     {
-        'author':'Khatry',
-        'title':'Second Post',
-        'content':'Hey Corey!',
-        'date_posted':'August 9,2023'
-    },
-        {
-        'author':'Nirjal',
-        'title':'Painful Post',
-        'content':'Teeth hurting',
-        'date_posted':'August 9,2023'
-    },
-]
-
-
+from django.shortcuts import render #loads HTML template with data and returns it as a web response
+from .models import Post # the dot before models is used as the models file is in the same directory(package)
 
 def home(request):
     context={
-        'posts':posts
+        'posts': Post.objects.all()
     }
-    return render (request,'blog/home.html',context)
+    return render (request,'blog/home.html',context) #parameters are requst (HTTP request sent by user's browser to build the response properly), template (the HTML file you want to display) path, context(Python dictionary that u want to send to the file, only a single dictionary)
 
 def about(request):
     return render (request,'blog/about.html',{'title':'About'})
